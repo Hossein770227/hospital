c6f459f9628460c2e39b101dd2ba1ab49b3f5605
hossein <hossein.h.a770227@gmail.com>
2025-09-18 19:50:03 +0330

    add docker

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'hospital'),  
+        'USER': os.environ.get('DATABASE_USER', 'root'),  
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'MYPASSWORD'), 
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),  
+        'PORT': int(os.environ.get('DATABASE_PORT', 5432)),  
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
