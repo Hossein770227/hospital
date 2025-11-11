c620b10947315e90b164bdd1fb2537fb3af071b9
hossein <hossein.h.a770227@gmail.com>
2025-11-11 19:35:17 +0330

    add postgresql database

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'hospital'),  
+        'USER': os.environ.get('DATABASE_USER', 'root'),  
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'MYPASSWORD'), 
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),  
+        'PORT': int(os.environ.get('DATABASE_PORT', 5432)),  
     }
 }
 
@@ -114,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
