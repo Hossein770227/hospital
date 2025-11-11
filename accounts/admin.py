4c71b8a9783d039343fd1e06a3802c1d6a48d12a
hossein <hossein.h.a770227@gmail.com>
2025-11-11 20:33:55 +0330

    add admin for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,54 @@
+import jdatetime
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .forms import UserCreationForm, UserChangeForm
+from .models import MyUser, OtpCode
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ["full_name", "phone_number", "is_active", "is_admin"]
+    list_filter = ["is_admin", "is_active", "is_staff"]
+
+    fieldsets = [
+        (None, {"fields": ["full_name", "phone_number", "password"]}),
+        (_("Permissions"), {
+            "fields": [
+                "is_active", "is_admin", "is_staff", "is_superuser",
+                "groups", "user_permissions"
+            ]
+        }),
+        (_("Important dates"), {"fields": ["last_login"]}),
+    ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["full_name", "phone_number", "password1", "password2", "is_active", "is_admin"],
+            },
+        ),
+    ]
+
+    search_fields = ["full_name", "phone_number"]
+    ordering = ["full_name"]
+    filter_horizontal = ["groups", "user_permissions"]
+
+
+admin.site.register(MyUser, UserAdmin)
+
+
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin):
+    list_display = ["phone_number", "code", "date_time_persian"]
+
+    def date_time_persian(self, obj):
+        return jdatetime.datetime.fromgregorian(datetime=obj.date_time_created).strftime("%Y/%m/%d %H:%M:%S")
+
+    date_time_persian.short_description = _("Date/Time (Persian)")
+    date_time_persian.admin_order_field = "date_time_created"
--- accounts/validators.py
+++ accounts/validators.py
@@ -1,10 +1,16 @@
 import re
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext as _ 
+from django.utils.translation import gettext_lazy as _
 
 
-phone_regex = re.compile(r"^(?:\+98|0)9\d{2}\d{7}$")
+phone_regex = re.compile(r"^(?:\+98|0)9\d{9}$")
 
 def validate_iranian_phone(value):
+
     if not phone_regex.match(value):
         raise ValidationError(_('Enter a valid Iranian cellphone number.'))
+    
+
+    if value.startswith('+98'):
+        value = '0' + value[3:]
+    return value
