3b7cbdf21f9540442019a4d02b50cf38fac67285
hossein <hossein.h.a770227@gmail.com>
2025-11-15 21:29:28 +0330

    add doctor model,add  Specialization model, addDoctorAdmin,

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import Specialization, Doctor
 
-# Register your models here.
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display =['full_name', 'medical_license_number', 'is_active', 'date_birth']
+    list_filter = ['full_name', 'is_active', 'date_birth']
+    search_fields = ['full_name', 'medical_license_number', 'specializations']
+    ordering = ['specializations']
+
+  
+    
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+class Specialization(models.Model):
+    title = models.CharField(_("specialization title"), max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = _("Specialization")
+        verbose_name_plural = _("Specializations")
+
+    def __str__(self):
+        return self.title
+
+
+class Doctor(models.Model):
+    full_name = models.CharField(_("full name"),max_length=100)
+    medical_license_number = models.CharField(_("medical license number"), max_length=50,unique=True)
+    bio = models.TextField(_("biography"), blank=True)
+    specializations = models.ManyToManyField(Specialization, verbose_name=_("specializations"))
+    is_active = models.BooleanField( _("is active"), default=True,)
+    date_birth =models.DateField(_("date birth"), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Doctor")
+        verbose_name_plural = _("Doctors")
+        ordering = ['full_name'] 
+
+    def __str__(self):
+        return f"name :{self.full_name} ({self.medical_license_number})"
